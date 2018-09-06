97fcd65aee1f92b08a3867ff9821f949dad23c67
Ophir LOJKINE <o.lojkine@qwant.com>
2018-09-06 10:44:37 +0200

    Implement bold

--- index.js
+++ index.js
@@ -6,8 +6,25 @@ const Saxophone = require('saxophone');
 function html2unicode(html) {
 	const chunks = [];
 	const parser = new Saxophone();
+	let tags = {"i": 0, "b": 0, "em":0, "strong": 0};
+	parser.on('tagopen', ({ name, attrs, isSelfClosing }) => {
+		if (!isSelfClosing && tags.hasOwnProperty(name)) {
+			tags[name]++;
+		}
+	});
+	parser.on('tagclose', ({ name }) => {
+		if (tags.hasOwnProperty(name)) {
+			tags[name]--;
+		}
+	});
+	const state = {
+		bold: false,
+		italics: false,
+	};
 	parser.on('text', ({ contents }) => {
-		chunks.push(contents);
+		state.bold = (tags.b || tags.strong) > 0;
+		state.italics = (tags.i || tags.em) > 0;
+		chunks.push(transform(contents, state));
 	});
 	const result = new Promise((resolve, reject) => {
 		parser.on('finish', () => {
@@ -18,7 +35,44 @@ function html2unicode(html) {
 	return result;
 }
 
+/**
+ * Transform a text into italics or bold
+ **/
+function transform(text, {bold, italics}) {
+	if (bold) text = bolden(text);
+	return text;
+}
+
+let charCodes = {};
+for(let l of ['a','z','A','Z','0','9']) {
+	charCodes[l] = l.charCodeAt(0);
+}
+
+function isCapitalLetter(code) {
+	return code >= charCodes.A && code <= charCodes.Z;
+}
+
+function isSmallLetter(code) {
+	return code >= charCodes.a && code <= charCodes.z;
+}
+
+function bolden(text) {
+	let codes = [];
+	for(let i=0; i<text.length; i++) {
+		let code = text.charCodeAt(i);
+		if (isCapitalLetter(code)) {
+			codes.push(0xD835);
+			codes.push(0xDDD4 + code - charCodes.A);
+		} else if (isSmallLetter(code)) {
+			codes.push(0xD835);
+			codes.push(0xDDEE + code - charCodes.a);
+		} else {
+			codes.push(code);
+		}
+	}
+	return String.fromCharCode(...codes);
+}
 
 if (typeof module !== "undefined") {
-	module.exports = { html2unicode };
+	module.exports = { html2unicode, transform, bolden };
 }
--- test.js
+++ test.js
@@ -6,6 +6,12 @@ async function test(html, txt) {
 	assert.strictEqual(res, txt);
 }
 
+// bold
 test("hello", "hello");
 test("<b>hello</b>", "𝗵𝗲𝗹𝗹𝗼");
-
+test("<b>Hello</b>", "𝗛𝗲𝗹𝗹𝗼");
+test("he<b>ll</b>o", "he𝗹𝗹o");
+test("<b>Hello, world!</b>", "𝗛𝗲𝗹𝗹𝗼, 𝘄𝗼𝗿𝗹𝗱!");
+test("<strong>Hello, world!</strong>", "𝗛𝗲𝗹𝗹𝗼, 𝘄𝗼𝗿𝗹𝗱!");
+test("<strong>Hello, <b>world<b>!</b></b></strong>", "𝗛𝗲𝗹𝗹𝗼, 𝘄𝗼𝗿𝗹𝗱!");
+test("<b>по-русский</b>", "по-русский");
